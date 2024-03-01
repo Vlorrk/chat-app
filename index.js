d9f1d81191543fe21537c4a8306856b73a63092d
javi <bzjvr@hotmail.com>
2024-02-29 19:55:17 -0800

    fix: fixed routing issued with index and login.html

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io');
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-
-
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, './public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "./public/main.html"));
 });
 
+io.on("connection", (socket) => {
 
+  socket.on("chat message", (msg) => {
+    const messageWithSender = { message: msg, sender: socket.id };
+    socket.broadcast.emit("chat message", messageWithSender);
 
-io.on('connection', (socket) => {
-
-  socket.on('chat message', (msg) => {
-    const messageWithSender = { message: msg, sender: socket.id }
-    socket.broadcast.emit('chat message', messageWithSender);
-
-    socket.emit('chat message', messageWithSender);
-  })
+    socket.emit("chat message", messageWithSender);
+  });
 });
 
 server.listen(3000, () => {
-  console.log('server is running on localhost:3000');
+  console.log("server is running on localhost:3000");
 });
