8e274609ab19ba18d4becb65c5eb53d0167de1bb
javi <bzjvr@hotmail.com>
2024-03-01 19:21:57 -0800

    feat:added sound and speech bubbles!

--- index.js
+++ index.js
@@ -7,19 +7,53 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+let username;
+let userSockets = {};
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
+  res.sendFile(join(__dirname, "./public/login.html"));
+});
+
+app.get("/chat", (_req, res) => {
   res.sendFile(join(__dirname, "./public/main.html"));
+});
+app.post("/login", (req, res) => {
+  username = req.body.username;
+
+  if (username) {
+    userSockets[username] = null;
+    res.redirect("/chat");
+  }
 });
 
 io.on("connection", (socket) => {
-
+  if (username && !userSockets[username]) {
+    userSockets[username] = socket.id;
+    io.emit("chat message", {
+      message: `${username} has connected!`,
+      sender: "server",
+    });
+  }
   socket.on("chat message", (msg) => {
     const messageWithSender = { message: msg, sender: socket.id };
     socket.broadcast.emit("chat message", messageWithSender);
 
     socket.emit("chat message", messageWithSender);
+  });
+
+  socket.on("disconnect", () => {
+    if (username && userSockets[username] === socket.id) {
+      userSockets[username] = null;
+
+      const disconnectMessage = {
+        message: `${username} has left that chat.`,
+        sender: "server",
+      };
+      io.emit("chat message", disconnectMessage);
+    }
   });
 });
 
--- public/app.js
+++ public/app.js
@@ -1,34 +1,41 @@
 const socket = io();
 
-const form = document.getElementById('form');
-const input = document.getElementById('input');
-const messages = document.getElementById('list');
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const messages = document.getElementById("list");
+const messageSound = document.getElementById("messageSound");
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   if (input.value) {
-    socket.emit('chat message', { message: input.value });
-    input.value = '';
-
+    socket.emit("chat message", input.value);
+    input.value = "";
   }
-})
+});
 
-
-socket.on('chat message', (msg) => {
-  const item = document.createElement('li');
-
+socket.on("chat message", (msg) => {
+  const item = document.createElement("li");
 
   if (msg.sender === socket.id) {
-    item.classList.add('sent-by-me')
+    item.classList.add("shared", "sent-by-me");
+  } else if (msg.sender === "server") {
+    item.classList.add("sent-by-server");
+  } else {
+    item.classList.add("shared", "sent-by-others");
   }
-  else {
-    item.classList.add('sent-by-others')
-  }
-  const messageText = msg.message.message;
+  const messageText = msg.message;
   item.textContent = messageText;
   messages.appendChild(item);
 
+  if (msg.sender != "server") {
+    playMessageSounds();
+  }
   //scroll to bottom after new message
   messages.scrollTop = messages.scrollHeight;
-})
+});
+
+function playMessageSounds() {
+  messageSound.volume = 0.1;
+  messageSound.play();
+}
